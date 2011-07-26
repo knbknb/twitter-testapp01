0a20b9371ac44edf8a252fe64438daee6c5bf5cb
Knut Behrends <knb@tnp-online.de>
2011-07-26 12:51:34 +0200

    initial commit, control file

---
+++ checktwitter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+start="788"  
+step="60"  # api calls capped: max 60 calls per hour 
+cnt=`wc -l contacts.txt | cut -d" " -f1`
+IFS=$'\n'
+
+while [ "$start" -le "$cnt" ]; do 
+
+ for user in `tail -n +$start contacts.txt | head -n $step `; do
+ 
+  cmd="perl ./test-search.pl --user=\"$user\" | tee -a twitterusers.txt"
+  echo $cmd
+  perl ./test-search.pl --user="$user" | tee -a twitterusers.txt
+  echo
+  echo
+ done
+start=`expr $start + $step`
+
+# sleep for more than 1 hour
+sleep 3700 
+done;
+
